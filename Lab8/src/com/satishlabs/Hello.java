c0bfccb97a52cb5e60434b2f1945c18fd28dc72c
Satish <spd61185@gmail.com>
2021-05-15 21:57:42 +0530

    Lab8

---
+++ Lab8/src/com/satishlabs/Hello.java
@@ -0,0 +1,16 @@
+package com.satishlabs;
+
+public class Hello {
+	private A aobj;
+	private B bobj;
+	
+	public Hello() {
+		System.out.println("Hello - ()");
+	}
+	
+	public void show() {
+		System.out.println(aobj);
+		System.out.println(bobj);
+	}
+	
+}
