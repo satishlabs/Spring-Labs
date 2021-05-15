3f0493dbd640116abae12d409de03dde3af6a89c
Satish <spd61185@gmail.com>
2021-05-15 22:01:59 +0530

    Lab9

---
+++ Lab9/src/com/satishlabs/Lab9.java
@@ -0,0 +1,11 @@
+package com.satishlabs;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Lab9 {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		Hello hello = (Hello)ctx.getBean("hello");
+		hello.show();	}
+}
