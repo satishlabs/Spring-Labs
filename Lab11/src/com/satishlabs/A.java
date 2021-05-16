57267d1afb2858b2d824d9d6d3e75513645d2471
Satish Prasad <spd61185@gmail.com>
2021-05-17 05:07:42 +0530

    Lab11

---
+++ Lab11/src/com/satishlabs/A.java
@@ -0,0 +1,16 @@
+package com.satishlabs;
+
+public class A {
+	private int a;
+	private String msg;
+	public void setA(int a) {
+		this.a = a;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	public String toString() {
+		return a+"\t"+msg;
+	}
+}
