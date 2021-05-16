67371e6671188c74e82341f57b6573954dfa93cb
Satish Prasad <spd61185@gmail.com>
2021-05-17 05:08:24 +0530

    Lab13

---
+++ Lab13/src/com/satishlabs/Hello.java
@@ -0,0 +1,18 @@
+package com.satishlabs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Hello {
+	@Autowired
+	private A aobj;
+	
+	@Autowired
+	private B bobj;
+	
+
+	public void show() {
+		System.out.println(aobj);
+		System.out.println(bobj);
+	}
+	
+}
