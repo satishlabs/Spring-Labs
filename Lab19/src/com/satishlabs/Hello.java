8c0d196d636ea829fbcfd910aec53a0423021cd8
Satish Prasad <spd61185@gmail.com>
2021-06-05 07:40:47 +0530

    Lab19

---
+++ Lab19/src/com/satishlabs/Hello.java
@@ -0,0 +1,18 @@
+package com.satishlabs;
+
+import javax.annotation.Resource;
+
+public class Hello {
+	@Resource
+	private A aobj;
+	
+	@Resource(name = "bo")
+	private B bobj;
+	
+
+	public void show() {
+		System.out.println(aobj);
+		System.out.println(bobj);
+	}
+	
+}
