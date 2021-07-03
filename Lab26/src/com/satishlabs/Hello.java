70067374aff698c5515a700bfba205cf363cf599
Satish Prasad <spd61185@gmail.com>
2021-07-03 23:18:15 +0530

    Lab26

--- Lab26/src/com/satishlabs/Hello.java
+++ Lab26/src/com/satishlabs/Hello.java
@@ -1,5 +1,16 @@
 package com.satishlabs;
 
-public class Hello {
+import org.springframework.beans.factory.annotation.Qualifier;
 
+public class Hello {
+	@Inject
+	private A aobj;
+	
+	@Inject
+	private B bobj;
+	
+	public void show() {
+		System.out.println(aobj);
+		System.out.println(bobj);
+	}
 }
