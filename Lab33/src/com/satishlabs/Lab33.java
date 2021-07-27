0284cff769b1a0b650613f96aba4915461962726
Satish Prasad <spd61185@gmail.com>
2021-07-27 15:40:22 +0530

    Lab33

--- Lab33/src/com/satishlabs/Lab33.java
+++ Lab33/src/com/satishlabs/Lab33.java
@@ -1,5 +1,13 @@
 package com.satishlabs;
 
-public class Lab33 {
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Lab33 {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MyConfig.class);
+		System.err.println("New Spring Container is Ready");
+		Hello h = ctx.getBean(Hello.class);
+		h.show();
+		ctx.registerShutdownHook();
+	}
 }
