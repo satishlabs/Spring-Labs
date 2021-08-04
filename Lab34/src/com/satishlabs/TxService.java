0debd4ee38e22740f89ed35defd994563bf860ae
Satish Prasad <spd61185@gmail.com>
2021-08-04 12:29:08 +0530

    TX Services classed added

---
+++ Lab34/src/com/satishlabs/TxService.java
@@ -0,0 +1,13 @@
+package com.satishlabs;
+
+public class TxService {
+	public void begin() {
+		System.out.println("** TS - begin");
+	}
+	public void commit() {
+		System.out.println("** TS - commit");
+	}
+	public void rollback() {
+		System.out.println("** TS - rollback");
+	}
+}
