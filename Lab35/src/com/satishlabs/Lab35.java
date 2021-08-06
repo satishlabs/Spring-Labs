5227a73d9f4ebd04f97616e4df4087386dc77693
Satish Prasad <spd61185@gmail.com>
2021-08-06 09:10:44 +0530

    Main class added

--- Lab35/src/com/satishlabs/Lab35.java
+++ Lab35/src/com/satishlabs/Lab35.java
@@ -1,7 +1,23 @@
 package com.satishlabs;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Lab35 {
 	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		CustomerService cs = (CustomerService) ctx.getBean("cs");
+		cs.addCustomer();
+		cs.updateCustomer();
 		
+		AccountService as = (AccountService) ctx.getBean("as");
+		as.mydeposit();
+		as.getBal();
+		try {
+			as.mywithdrow();
+		}catch (Exception e) {
+			System.out.println("Sorry ----");
+			System.out.println(e);
+		}
 	}
 }
