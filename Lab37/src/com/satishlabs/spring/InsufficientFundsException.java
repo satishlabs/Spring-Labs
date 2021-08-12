f27f9f126d0c3c4e96802533276d07492d0a225d
Satish Prasad <spd61185@gmail.com>
2021-08-12 20:43:19 +0530

    Updated

---
+++ Lab37/src/com/satishlabs/spring/InsufficientFundsException.java
@@ -0,0 +1,10 @@
+package com.satishlabs.spring;
+
+public class InsufficientFundsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6293281178209007953L;
+
+}
