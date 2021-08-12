3edda67ee84fcea5489c36380f9569e8178cdafb
Satish Prasad <spd61185@gmail.com>
2021-08-12 20:45:16 +0530

    Main class updated

--- Lab37/src/com/satishlabs/spring/Lab37.java
+++ Lab37/src/com/satishlabs/spring/Lab37.java
@@ -1,5 +1,28 @@
 package com.satishlabs.spring;
 
-public class Lab37 {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
+
+public class Lab37 {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		CustomerService cs = (CustomerService) ctx.getBean("cs");
+		cs.addCustomer();
+		System.out.println("----- ----");
+		cs.updateCustomer();
+		System.out.println("----- ----");
+		
+		AccountService as = (AccountService) ctx.getBean("as");
+		as.mydeposit();
+		System.out.println("----- ----");
+		as.getBal();
+		System.out.println("----- ----");
+		try {
+			as.mywithdrow();
+		}catch (Exception e) {
+			System.out.println("Sorry --- "+e);
+		}
+	}
 }
