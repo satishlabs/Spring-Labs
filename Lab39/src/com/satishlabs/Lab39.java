aa3ef8895cdee15e11506fca36256c8ea8b3016d
Satish Prasad <spd61185@gmail.com>
2021-08-17 17:28:24 +0530

    Update configuration and main class

--- Lab39/src/com/satishlabs/Lab39.java
+++ Lab39/src/com/satishlabs/Lab39.java
@@ -1,7 +1,26 @@
 package com.satishlabs;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Lab39 {
 	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		CustomerService cs = (CustomerService) ctx.getBean("cs");
+		cs.addCustomer();
+		System.out.println("----- ----");
+		cs.updateCustomer();
+		System.out.println("----- ----");
 		
+		AccountService as = (AccountService) ctx.getBean("as");
+		as.mydeposit();
+		System.out.println("----- ----");
+		as.getBal();
+		System.out.println("----- ----");
+		try {
+			as.mywithdrow();
+		}catch (Exception e) {
+			System.out.println("Sorry --- "+e);
+		}
 	}
 }
