4134faeb0a9e3557c3c7ced62f23ed3e9d8e5c62
Satish Prasad <spd61185@gmail.com>
2021-09-04 20:56:19 +0530

    Main class updated

--- Lab40/src/com/satishlabs/Lab40.java
+++ Lab40/src/com/satishlabs/Lab40.java
@@ -1,7 +1,15 @@
 package com.satishlabs;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.satishlabs.jdbc.TestService;
+
 public class Lab40 {
 	public static void main(String[] args) {
-		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		TestService ts = (TestService) ctx.getBean("ts");
+		ts.showOracleInfo();
+		ts.showMySQLInfo();
 	}
 }
