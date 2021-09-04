791a71aad3e0ad8be0a4b75662dc32cd2ce39771
Satish Prasad <spd61185@gmail.com>
2021-09-04 20:43:09 +0530

    showOracleInfo

--- Lab40/src/com/satishlabs/jdbc/TestService.java
+++ Lab40/src/com/satishlabs/jdbc/TestService.java
@@ -1,7 +1,12 @@
 package com.satishlabs.jdbc;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+
+
 
 public class TestService {
 	@Resource(name = "oracleDS")
@@ -9,4 +14,14 @@ public class TestService {
 	
 	@Resource(name = "mysqlDS")
 	private DataSource mysqlDS;
+	
+	public void showOracleInfo() {
+		try {
+			Connection oracon = oracleDS.getConnection();
+			DatabaseMetaData oradbmd = oracon.getMetaData();
+			System.out.println(oradbmd.getDefaultTransactionIsolation());
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
