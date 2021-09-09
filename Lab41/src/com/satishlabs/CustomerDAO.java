f65ad35b06f11d0100fe187ce3c483f0e72cf1e1
Satish Prasad <spd61185@gmail.com>
2021-09-09 05:34:24 +0530

    Added entity

--- Lab41/src/com/satishlabs/CustomerDAO.java
+++ Lab41/src/com/satishlabs/CustomerDAO.java
@@ -1,5 +1,7 @@
 package com.satishlabs;
 
 public interface CustomerDAO {
-	
+	public void addCustomer(CustomerTO cto);
+	public void updateCustomer(CustomerTO cto);
+	public void deleteCustomer(int cid);
 }
