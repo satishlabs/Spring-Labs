b4c7d39c3822dc3cdc58cae6133dcfe3b712f97a
Satish Prasad <spd61185@gmail.com>
2021-09-14 08:39:49 +0530

    Main class updated

--- Lab43/src/com/satishlabs/Lab43.java
+++ Lab43/src/com/satishlabs/Lab43.java
@@ -1,5 +1,24 @@
 package com.satishlabs;
 
-public class Lab43 {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class Lab43 {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		
+		CustomerDAO cdao = (CustomerDAO) ctx.getBean("cdao");
+		
+		//Add Customer
+		CustomerTO cto = new CustomerTO(103, "Mantu", "man@gmail", 3333, "Patna");
+		cdao.addCustomer(cto);
+		
+		//2. Update Customer
+		CustomerTO cto1 = new CustomerTO(102, "Kaushik", "man@gmail", 010101, "Patna");
+		cdao.updateCustomer(cto1);
+		
+		//3. Delete Customer
+		cdao.deleteCustomer(103);
+		System.out.println("Check your database");
+	}
 }
