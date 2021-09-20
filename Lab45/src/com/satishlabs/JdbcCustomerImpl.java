6108f73770740077263e6b8bb59b4ec343f7336c
Satish Prasad <spd61185@gmail.com>
2021-09-20 08:41:48 +0530

    addCustomer method implemented

--- Lab45/src/com/satishlabs/JdbcCustomerImpl.java
+++ Lab45/src/com/satishlabs/JdbcCustomerImpl.java
@@ -1,5 +1,37 @@
 package com.satishlabs;
 
-public class JdbcCustomerImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+public class JdbcCustomerImpl extends JdbcDaoSupport implements CustomerDAO{
+
+	@Override
+	public void addCustomer(CustomerTO cto) {
+		PreparedStatementCreator psc = new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement ps = con.prepareStatement("insert into customers values(?,?,?,?,?)");
+				ps.setInt(1, cto.getCid());
+				ps.setString(2, cto.getCname());
+				ps.setString(3, cto.getEmail());
+				ps.setString(4, cto.getCity());
+				ps.setLong(5, cto.getPhone());
+				return ps;
+			}
+		};
+		getJdbcTemplate().update(psc);
+	}
+
+	@Override
+	public List<CustomerTO> getAllCustomers() {
+		
+		return null;
+	}
+	
 }
--- Lab45/src/com/satishlabs/Lab45.java
+++ Lab45/src/com/satishlabs/Lab45.java
@@ -1,7 +1,15 @@
 package com.satishlabs;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Lab45 {
 	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		CustomerDAO cdao = (CustomerDAO) ctx.getBean("cdao");
 		
+		//1. Add Customer
+		CustomerTO cto = new CustomerTO(301, "Test", "test@gmail", 9999, "Test City");
+		cdao.addCustomer(cto);
 	}
 }
