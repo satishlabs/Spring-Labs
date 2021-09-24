0c2d7626bb1f00ea058a65f3afbc30cebe54999e
Satish Prasad <spd61185@gmail.com>
2021-09-24 20:06:11 +0530

    Interface impl added

---
+++ Lab46/src/com/satishlabs/JdbcCustomerImpl.java
@@ -0,0 +1,45 @@
+package com.satishlabs;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+public class JdbcCustomerImpl extends JdbcDaoSupport implements CustomerDAO{
+
+	@Override
+	public void addCustomer(CustomerTO cto) {
+		PreparedStatementCreator psc = new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement ps = con.prepareStatement("insert into customers values(?,?,?,?,?)");
+				ps.setInt(1, cto.getCid());
+				ps.setString(2, cto.getCname());
+				ps.setString(3, cto.getEmail());
+				ps.setString(4, cto.getCity());
+				ps.setLong(5, cto.getPhone());
+				return ps;
+			}
+		};
+		getJdbcTemplate().update(psc);
+	}
+
+	@Override
+	public List<CustomerTO> getAllCustomers() {
+		PreparedStatementCreator psc = new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement ps = con.prepareStatement("select * from customers");
+				return ps;
+			}
+		};
+		List list = getJdbcTemplate().query(psc, new CustomerRowMapper());
+		return list;
+	}
+	
+}
