22983dbd4cad3628db99e76854940900dfc3d2ef
Satish Prasad <spd61185@gmail.com>
2021-09-27 22:53:03 +0530

    Implementaion modified

--- Lab46/src/com/satishlabs/JdbcCustomerImpl.java
+++ Lab46/src/com/satishlabs/JdbcCustomerImpl.java
@@ -1,45 +1,57 @@
 package com.satishlabs;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.CallableStatementCreator;
+import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 public class JdbcCustomerImpl extends JdbcDaoSupport implements CustomerDAO{
 
 	@Override
 	public void addCustomer(CustomerTO cto) {
-		PreparedStatementCreator psc = new PreparedStatementCreator() {
+		CallableStatementCreator csc = new CallableStatementCreator() {
 			
 			@Override
-			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				PreparedStatement ps = con.prepareStatement("insert into customers values(?,?,?,?,?)");
-				ps.setInt(1, cto.getCid());
-				ps.setString(2, cto.getCname());
-				ps.setString(3, cto.getEmail());
-				ps.setString(4, cto.getCity());
-				ps.setLong(5, cto.getPhone());
-				return ps;
+			public CallableStatement createCallableStatement(Connection con) throws SQLException {
+				CallableStatement cs = con.prepareCall("call addCustomer(?,?,?,?,?)");
+				cs.setInt(1, cto.getCid());
+				cs.setString(2, cto.getCname());
+				cs.setString(3, cto.getEmail());
+				cs.setLong(4, cto.getPhone());
+				cs.setString(5, cto.getCity());
+				return cs;
 			}
 		};
-		getJdbcTemplate().update(psc);
+		SqlParameter charParam = new SqlParameter(Types.VARCHAR);
+		SqlParameter longParam = new SqlParameter(Types.LONGNVARCHAR);
+		
+		List<SqlParameter> plist = new ArrayList<SqlParameter>();
+		plist.add(charParam);
+		plist.add(charParam);
+		plist.add(charParam);
+		plist.add(longParam);
+		plist.add(charParam);
+		
+		getJdbcTemplate().call(csc, plist);
 	}
 
 	@Override
-	public List<CustomerTO> getAllCustomers() {
-		PreparedStatementCreator psc = new PreparedStatementCreator() {
-			
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				PreparedStatement ps = con.prepareStatement("select * from customers");
-				return ps;
-			}
-		};
-		List list = getJdbcTemplate().query(psc, new CustomerRowMapper());
-		return list;
+	public void updateCustomer(CustomerTO cto) {
+		// TODO Auto-generated method stub
+		
 	}
-	
+
+	@Override
+	public void deleteCustomer(int cid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
