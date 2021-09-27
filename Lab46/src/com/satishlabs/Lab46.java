ae7eeb949645259ff387f9200027761da00b1743
Satish Prasad <spd61185@gmail.com>
2021-09-27 22:55:57 +0530

    Main class updated

--- Lab46/src/com/satishlabs/Lab46.java
+++ Lab46/src/com/satishlabs/Lab46.java
@@ -1,7 +1,16 @@
 package com.satishlabs;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Lab46 {
 	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		CustomerDAO cdao = (CustomerDAO) ctx.getBean("cdao");
 		
+		//1. Add Customer
+		CustomerTO cto = new CustomerTO(109, "Testee", "test111@g", 1010, "Delhi");
+		cdao.addCustomer(cto);
+		System.out.println("Call Complete");
 	}
 }
