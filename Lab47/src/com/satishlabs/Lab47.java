afc7c89ed8ebc09f429f3057056a05d5d352c292
Satish Prasad <spd61185@gmail.com>
2021-10-02 22:28:25 +0530

    AddCustomer updated

--- Lab47/src/com/satishlabs/Lab47.java
+++ Lab47/src/com/satishlabs/Lab47.java
@@ -1,5 +1,15 @@
 package com.satishlabs;
 
-public class Lab47 {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class Lab47 {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		CustomerDAO cdao = (CustomerDAO) ctx.getBean("cdao");
+		
+		//1. Add Customer
+		CustomerTO cto = new CustomerTO(401, "KumarTest", "kt@gmail", 98989, "Patna");
+		cdao.addCustomer(cto);
+	}
 }
