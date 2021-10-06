bacb6540f2e66e8aebd6b4bd08e0d852cbd8ad04
Satish Prasad <spd61185@gmail.com>
2021-10-06 08:23:35 +0530

    Main class updated

--- Lab49/src/com/satishlabs/hibernate/Lab49.java
+++ Lab49/src/com/satishlabs/hibernate/Lab49.java
@@ -1,7 +1,27 @@
 package com.satishlabs.hibernate;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Lab49 {
 	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		CustomerDAO cdao =(CustomerDAO) ctx.getBean("cdao");
 		
+		//1. Add Customer
+		CustomerTO cto = new CustomerTO(401, "Srini", "srini@gmail", 1234, "PU");
+		cdao.addCustomer(cto);
+		
+		//2. Update Customer
+		CustomerTO cto1 = new CustomerTO(203, "Srini", "srini@gmail", 1234, "PU");
+		cdao.updateCustomer(cto1);
+		
+		//3. delete Customer
+		cdao.deleteCustomer(206); 
+		
+		//4. Get Customer By Cid
+		System.out.println("Get Customer by Cid");
+		cto = cdao.getCustomerByCid(301);
+		System.out.println(cto);
 	}
 }
