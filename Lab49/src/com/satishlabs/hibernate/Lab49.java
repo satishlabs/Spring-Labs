6577f77e7f9a4c797ee348b737272573810eb866
Satish Prasad <spd61185@gmail.com>
2021-10-08 16:28:36 +0530

    Main class updated

--- Lab49/src/com/satishlabs/hibernate/Lab49.java
+++ Lab49/src/com/satishlabs/hibernate/Lab49.java
@@ -1,5 +1,7 @@
 package com.satishlabs.hibernate;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,20 +10,38 @@ public class Lab49 {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
 		CustomerDAO cdao =(CustomerDAO) ctx.getBean("cdao");
 		
-		//1. Add Customer
-		CustomerTO cto = new CustomerTO(401, "Srini", "srini@gmail", 1234, "PU");
-		cdao.addCustomer(cto);
+		//1. Get all Customers
+		System.out.println("Get All Customers");
+		List<CustomerTO> list = cdao.getAllCustomers();
+		for(CustomerTO ct:list) {
+			System.out.println(ct);
+		}
 		
-		//2. Update Customer
-		CustomerTO cto1 = new CustomerTO(203, "Srini", "srini@gmail", 1234, "PU");
-		cdao.updateCustomer(cto1);
-		
-		//3. delete Customer
-		cdao.deleteCustomer(206); 
-		
-		//4. Get Customer By Cid
-		System.out.println("Get Customer by Cid");
-		cto = cdao.getCustomerByCid(301);
+		//2. Get Customer By Email
+		System.out.println("Get Customer by Email");
+		CustomerTO cto = cdao.getCustomerByEmail("satish@gmail");	
 		System.out.println(cto);
+		
+		//3. Get Customer By City
+		System.out.println("Get Customer by City");
+		list = cdao.getCustomersByCity("Ranchi");
+		for(CustomerTO c: list) {
+			System.out.println(c);
+		}
+		
+		//4. Get Customer Count
+		System.out.println("Get Customer Counts");
+		int count = cdao.getCustomersCount();
+		System.out.println("No of cust: "+count);
+		
+		//5. Get Customer City by Email
+		System.out.println("Get Customer City By Email");
+		String ct = cdao.getCustomerCityByEmail("abc@gmail");
+		System.out.println(ct);
+		
+		//6. Get Customer Phone by Email
+		System.out.println("Get Customer Phone by EMail");
+		Long cp = cdao.getCustomerPhoneByEmail("abc@gmail");
+		System.out.println(cp);
 	}
 }
