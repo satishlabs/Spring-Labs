98523e4f71bdb42e4aa76e0b6a83afb92483f419
Satish Prasad <spd61185@gmail.com>
2021-11-12 08:23:38 +0530

    Main class updated

--- Lab51/src/com/satishlabs/Lab51.java
+++ Lab51/src/com/satishlabs/Lab51.java
@@ -1,7 +1,16 @@
 package com.satishlabs;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Lab51 {
 	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satish.xml");
+		CustomerDAO cdao = (CustomerDAO) ctx.getBean("cdao");
 		
+		//1. Add Customer
+		CustomerTO cto = new CustomerTO(109, "Satish", "sati@gmail", 12345, "Ranchi");
+		cdao.addCustomer(cto);
+		System.out.println("Check your databases");
 	}
 }
