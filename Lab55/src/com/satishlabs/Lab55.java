7c3e035f8b8a6e08be23ca10a0415dd477b0e7a7
Satish Prasad <spd61185@gmail.com>
2021-11-27 18:51:34 +0530

    Main class updated

--- Lab55/src/com/satishlabs/Lab55.java
+++ Lab55/src/com/satishlabs/Lab55.java
@@ -1,7 +1,33 @@
 package com.satishlabs;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Lab55 {
 	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satishlabs.xml");
+		AccountDAO adao = (AccountDAO) ctx.getBean("adao");
 		
+		System.out.println(adao.getBalance(101));
+		System.out.println(adao.getBalance(102));
+		
+		//1. deposit
+		adao.deposit(101, 20000.0);
+		
+		//2. withdraw
+		adao.withdraw(102, 50000.0);
+		
+		//3. get Balance
+		System.out.println(adao.getBalance(101));
+		System.out.println(adao.getBalance(102));
+		
+		//4. Funds Transfer
+		System.out.println(adao.getBalance(103));
+		System.out.println(adao.getBalance(101));
+		
+		adao.fundsTransfer(103, 101, 30000.0);
+		
+		System.out.println(adao.getBalance(103));
+		System.out.println(adao.getBalance(101));
 	}
 }
