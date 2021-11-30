43b2982267bec8f756a2c9b16f16ae819f9d688f
Satish Prasad <spd61185@gmail.com>
2021-11-30 18:57:10 +0530

    Deposit method implememted

--- Lab56/src/com/satishlabs/HibernateAccountDAO.java
+++ Lab56/src/com/satishlabs/HibernateAccountDAO.java
@@ -1,5 +1,45 @@
 package com.satishlabs;
 
-public class HibernateAccountDAO {
+import org.hibernate.LockMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+public class HibernateAccountDAO implements AccountDAO{
+	@Autowired
+	private HibernateTemplate hibernateTemp = null;
+	
+	@Autowired
+	private HibernateTransactionManager txManager = null;
+	
+	@Override
+	public void deposit(int accno, double amt) {
+		TransactionDefinition txDef = new DefaultTransactionDefinition();
+		TransactionStatus ts = txManager.getTransaction(txDef);
+		Account acc = hibernateTemp.load(Account.class,	accno, LockMode.READ);
+		acc.setBalance(acc.getBalance()+amt);
+		txManager.commit(ts);
+	}
+
+	@Override
+	public void withdraw(int accno, double amt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fundsTransfer(int accno, int daccno, double amt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getBalance(int accno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- Lab56/src/com/satishlabs/Lab56.java
+++ Lab56/src/com/satishlabs/Lab56.java
@@ -1,7 +1,17 @@
 package com.satishlabs;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Lab56 {
 	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("satishlabs.xml");
+		AccountDAO adao = (AccountDAO) ctx.getBean("adao");
 		
+		System.out.println(adao.getBalance(101));
+		System.out.println(adao.getBalance(102));
+		
+		//1. deposit
+		adao.deposit(101, 20000.0);
 	}
 }
